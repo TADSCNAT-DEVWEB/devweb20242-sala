639b40ac9d9ebae7d5af10649eb5c7757f51d1d4
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2024-11-01 19:02:30 +0000

    Implementação de nova rota durante o exercício de sala.

--- projpolls/polls/views.py
+++ projpolls/polls/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Bem-vindo à aplicação Enquete</h1>")
+def exibir(request):
+    return HttpResponse("<h1>Nova Rota de Exibição</h1>")
