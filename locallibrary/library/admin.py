2288739de7b59c1b229e62aa60a604b250cf1b2a
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2024-12-16 17:40:48 +0000

    Desenvolvimento das atividades em sala de aula.

--- locallibrary/library/admin.py
+++ locallibrary/library/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Language,Genre,Author,Book,Authorship
 
 # Register your models here.
+
+admin.site.register(Language)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Authorship)
--- locallibrary/library/models.py
+++ locallibrary/library/models.py
@@ -6,3 +6,42 @@ class Genre(models.Model):
     name=models.CharField(verbose_name="Nome",max_length=100)
     def __str__(self):
         return self.name
+
+class Language(models.Model):
+    name=models.CharField(verbose_name="Nome",max_length=200)
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+
+    GENDER=(('F','Female'),('M','Male'))
+
+    name=models.CharField(verbose_name="Nome",max_length=200)
+    dateOfBirth=models.DateField(verbose_name="Data de Nascimento")
+    dateOfDeath=models.DateField(verbose_name="Data de Falecimento",null=True,blank=True)
+    gender=models.CharField(max_length=1,choices=GENDER,blank=True,verbose_name="Gênero",
+                            help_text='Selecione o gênero da lista apresentada')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+
+    title=models.CharField(max_length=200)
+    summary=models.TextField(max_length=100,help_text="Informe o sumário do Livro",verbose_name="Sumário")
+    language=models.ForeignKey(Language,on_delete=models.SET_NULL,null=True,related_name="books")
+    isbn=models.CharField(verbose_name='ISBN',max_length=13,unique=True,help_text='Informe o <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN</a>')
+    authors=models.ManyToManyField(Author,related_name="books",help_text="Selecione os Autores do Livro",through='Authorship')
+    genres=models.ManyToManyField(Genre,related_name="books",help_text="Selecione os Gêneros do Livro")
+
+    def __str__(self):
+        return self.title
+
+class Authorship(models.Model):
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,verbose_name="Autor")
+    book=models.ForeignKey(Book,on_delete=models.CASCADE,verbose_name="Livro")
+    role=models.CharField(max_length=100,verbose_name="Papel")
+
+    def __str__(self):
+        return f'{self.book.title}-{self.role}'
