93a5095132622b29192683983f9baab3586a3a62
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2025-02-04 22:10:34 +0000

    Refatoração de classed based views.

--- projeventos/appeventos/urls.py
+++ projeventos/appeventos/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
-from .views import index,listar_eventos,listar_atividades,exibir_form_inscricao,registrar_inscricao,listar_participantes
+#from .views import index,listar_eventos,listar_atividades,exibir_form_inscricao,registrar_inscricao,listar_participantes
+from .views import EventoView,AtividadeView,AtividadeParticipanteView,InscricaoView
+from django.views.generic import TemplateView
 app_name='appeventos'
+#urlpatterns=[
+#    path('',index,name='index'),
+#    path('eventos/listar',listar_eventos,name='listar_eventos'),
+#    path('eventos/<int:evento_id>/atividades/listar',listar_atividades,name='listar_atividades'),
+#    path('eventos/atividades/<int:atividade_id>/inscrever',exibir_form_inscricao,name='exibir_form_inscricao'),
+#    path('eventos/atividades/<int:atividade_id>/registrar',registrar_inscricao,name="registrar_inscricao"),
+#    path('eventos/atividades/<int:atividade_id>/participantes',listar_participantes,name='listar_participantes')
+#]
+
 urlpatterns=[
-    path('',index,name='index'),
-    path('eventos/listar',listar_eventos,name='listar_eventos'),
-    path('eventos/<int:evento_id>/atividades/listar',listar_atividades,name='listar_atividades'),
-    path('eventos/atividades/<int:atividade_id>/inscrever',exibir_form_inscricao,name='exibir_form_inscricao'),
-    path('eventos/atividades/<int:atividade_id>/registrar',registrar_inscricao,name="registrar_inscricao"),
-    path('eventos/atividades/<int:atividade_id>/participantes',listar_participantes,name='listar_participantes')
-]
+    path('',TemplateView.as_view(template_name='appeventos/index.html'),name='index'),
+    path('eventos/listar',EventoView.as_view(),name='listar_eventos'),
+    path('eventos/<int:evento_id>/atividades/listar',AtividadeView.as_view(),name='listar_atividades'),
+    path('eventos/atividades/<int:atividade_id>/inscrever',InscricaoView.as_view(),name='exibir_form_inscricao'),
+    path('eventos/atividades/<int:atividade_id>/registrar',InscricaoView.as_view(),name="registrar_inscricao"),
+    path('eventos/atividades/<int:atividade_id>/participantes',AtividadeParticipanteView.as_view(),name='listar_participantes'),
+    path('eventos/atividades/<int:atividade_id>/registrar_presenca',AtividadeParticipanteView.as_view(),name='registrar_presenca'),]
